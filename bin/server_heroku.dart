45a56213293a2edbb9f94f76f7f81e1ed59a0a8e
Zdeněk Mlčoch <nachvilku01@centrum.cz>
2015-05-05 19:22:50 +0200

    h8

--- bin/server_heroku.dart
+++ bin/server_heroku.dart
@@ -6,7 +6,7 @@ import 'package:shelf/shelf.dart' as Shelf;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:path/path.dart' show join, dirname;
 import '../lib/shelf_web_socket/shelf_web_socket.dart' as sWs;
-import '../lib/shelf_static/shelf_static.dart';
+import '../lib/shelf_static/shelf_static.dart' as Static;
 import 'package:shelf_route/shelf_route.dart' as Route;
 import 'package:shelf_simple_session/shelf_simple_session.dart' as SimpleSession;
 import 'dart:async';
@@ -39,13 +39,13 @@ void main() {
   load();
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 80 : int.parse(portEnv);
-  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'web');
-  var staticHandler = createStaticHandler(pathToBuild, defaultDocument: 'index.html');
+  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'build/web');
+  var staticHandler = Static.createStaticHandler(pathToBuild, defaultDocument: 'index.html');
   middle = SimpleSession.sessionMiddleware(new SimpleSession.SimpleSessionStore());
 
   myRouter = Route.router();
-//  myRouter.get("/$CONTROLLER_DATA", data, middleware: middle);
-//  myRouter.get("/$CONTROLLER_WEBSOCKET", sWs.webSocketHandler(handleSockets), middleware: middle);
+  myRouter.get("/$CONTROLLER_DATA", data, middleware: middle);
+  myRouter.get("/$CONTROLLER_WEBSOCKET", sWs.webSocketHandler(handleSockets), middleware: middle);
 
   Shelf.Handler handler = new Shelf.Cascade().add(myRouter.handler).add(staticHandler).handler;
   io.serve(handler, InternetAddress.ANY_IP_V4, port).then((server) {
@@ -53,3 +53,122 @@ void main() {
   });
   }, onError: (e, stackTrace)=> print('Oh noes! $e $stackTrace'));
 }
+
+void handleSockets(socket, protocol, Shelf.Request request){
+  Player player = getPlayer(request);
+  player.socket = socket;
+  socket.listen((String message){
+    Map json = JSON.decode(message);
+    String action = json["action"];
+    Map data = json["data"];
+    Map outBox = {};
+    outBox["data"] = {};
+    Map out = outBox["data"];
+    String outAction;
+    switch(action){
+      case CONTROLLER_STATE:
+        outAction = getState(out, player, data);
+        break;
+      case ACTION_LOGIN:
+        outAction = login(out, player, data);
+        break;
+      case ACTION_HOST:
+        outAction = host(out, player, data);
+        break;
+      case ACTION_GAMES:
+        outAction = getGames(out, player, data);
+        break;
+      case ACTION_SELECTED_UNITS:
+        outAction = player.game.unitsSelected(out, player, data);
+        break;
+      case ACTION_ENTER_GAME:
+        outAction = enterGame(out, player, data);
+        break;
+      case ACTION_TRACK_CHANGE:
+        outAction = trackChange(out, player, data);
+        break;
+      case ACTION_NEXT_TURN:
+        outAction = newTurn(out, player, data);
+        break;
+    }
+    if(outAction==ACTION_DO_NOTHING)return;
+    outBox["state"] = player.state;
+    outBox["action"] = outAction;
+    outBox["origin"] = "main_handler";
+    socket.add(JSON.encode(outBox));
+  });
+}
+
+String newTurn(Map out, Player player, Map data){
+  player.game.nextTurn(player);
+  return ACTION_DO_NOTHING;
+}
+
+
+String trackChange(Map out, Player player, Map data){
+//  print("action trackChnge sent to ${player.game.opponent(player).nick}");
+  player.game.opponent(player)
+    ..sendMessage(ACTION_TRACK_CHANGE, data);
+  return ACTION_DO_NOTHING;
+}
+
+String enterGame(Map out, Player player, Map data){
+  String id = data["gameId"];
+  String raceId = data["race"];
+  player.race = races[raceId];
+  if(player.race==null){
+    player.race = races.values.first;
+  }
+  Game game = games.getGameById(id);
+  game.addPlayer(player);
+  return ACTION_DO_NOTHING;
+}
+
+
+String getGames(Map out, Player player, Map data){
+  out["games"] = games.toMatchmakingJson();
+  out["players"] = playersToMatchmakingJson();
+  return ACTION_GAMES;
+}
+
+String getState(Map out, Player player, _){
+  if(player.state == null){
+    player.state = STATE_NOT_LOGGED;
+  }
+  String state = player.state;
+  if(player.game != null){
+    out["game"] = player.game.toJson(player, player.state!=STATE_GAME);
+  }else{
+    out["player"] = player.toJson(player);
+  }
+  if(state == STATE_MATCHMAKING){
+    out["games"] = games.toMatchmakingJson();
+    out["players"] = playersToMatchmakingJson();
+  }
+  return ACTION_CHANGE_STATE;
+}
+
+String login(Map out, Player player, Map data){
+  String nick = data["nick"];
+  player.nick = nick;
+  player.state = STATE_MATCHMAKING;
+  return getState(out, player, data);
+}
+
+String host(Map out, Player player, Map data){
+  player.state = STATE_UNITS;
+  player.race = races[data["race"]];
+  if(player.race==null){
+    print("undefined race in host data ${JSON.encode(data)}");
+  }
+  Game newGame = games.createGame();
+  newGame.hostGame(data["gold"], player);
+  return getState(out, player, data);
+}
+
+Shelf.Response data(Shelf.Request request){
+  var headers = <String, String>{HttpHeaders.CONTENT_TYPE: "text/json", HttpHeaders.CONTENT_ENCODING: "utf8"};
+  File file = new File(PATH_TO_RESOURCES);
+  return new Shelf.Response.ok(file.openRead(), headers: headers);
+}
+
