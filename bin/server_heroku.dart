9c8b9067d0216c910e1ca287e33eb5d35468fca0
Zdeněk Mlčoch <nachvilku01@centrum.cz>
2015-05-05 18:58:04 +0200

    h7

--- bin/server_heroku.dart
+++ bin/server_heroku.dart
@@ -35,56 +35,21 @@ Shelf.Middleware middle;
 
 
 void main() {
+  runZoned((){
   load();
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 80 : int.parse(portEnv);
-
   var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'web');
-
-//  pool = new Pool(uri, minConnections: 1, maxConnections: 10,
-//      idleTimeout: const Duration(seconds:100),
-//      maxLifetime: const Duration(seconds:300),
-//      leakDetectionThreshold:const Duration(seconds:300));
-//  pool.messages.listen(print);
-//  pool.start().then((_) {
-//    pool.connect().then((conn) {
-//      conn.query("""
-//              select max(id) from "User"
-//          """).toList().then((List<Row> rows) {
-//        nextUserId = rows.first.toList().first.toInt() + 1;
-//      }).then((event) {
-//
-//        return conn.close();
-//      }) // Return connection to pool
-//      .catchError((err) => print('Query error: $err'));
-//      conn.close();
-//    });
-//  });
-
-
   var staticHandler = createStaticHandler(pathToBuild, defaultDocument: 'index.html');
+  middle = SimpleSession.sessionMiddleware(new SimpleSession.SimpleSessionStore());
 
+  myRouter = Route.router();
+//  myRouter.get("/$CONTROLLER_DATA", data, middleware: middle);
+//  myRouter.get("/$CONTROLLER_WEBSOCKET", sWs.webSocketHandler(handleSockets), middleware: middle);
 
-//  SimpleSessionStore store = new SimpleSessionStore();
-  Shelf.Middleware middle = SimpleSession.sessionMiddleware(new SimpleSession.SimpleSessionStore());
-
-  myRouter = Route.router()..get("/db", (Shelf.Request request) {
-    return new Shelf.Response.ok("db");
-  }, middleware: middle);
-
-
-  Shelf.Handler handler = new Shelf.Cascade().add(staticHandler).add(myRouter.handler).handler;
+  Shelf.Handler handler = new Shelf.Cascade().add(myRouter.handler).add(staticHandler).handler;
   io.serve(handler, InternetAddress.ANY_IP_V4, port).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
   });
-
-}
-
-void logout(StreamController controller, Shelf.Request request) {
-  Map mySession = SimpleSession.session(request);
-  mySession.remove("logged");
-  controller.add(const Utf8Codec().encode(JSON.encode({
-    "logout": true
-  })));
-  controller.close();
+  }, onError: (e, stackTrace)=> print('Oh noes! $e $stackTrace'));
 }
